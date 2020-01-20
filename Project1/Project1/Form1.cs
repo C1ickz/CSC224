f1f54c9093d044aae0f3e364a1266a960e6415e2
harrisrl2023 <46679450+harrisrl2023@users.noreply.github.com>
2020-01-20 16:29:05 -0500

    added comments and changed alignment

--- Project1/Project1/Form1.cs
+++ Project1/Project1/Form1.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/******************************
+ * CSC 224
+ * Programming Assignment # 1
+ * Author:  Ryan Harris
+ * Date:  January 20, 2020
+ ******************************/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +14,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+
 
 namespace Project1
 {
@@ -34,40 +43,53 @@ namespace Project1
             decimal interestEarned = 0m;
             decimal endingBal = annualInvest;
             int year = 2020;
-            
-            txtOutputArea.AppendText("IRA Summary Projection for " + txtName.Text + "\r\n\r\n");
-            txtOutputArea.AppendText("Fixed Annual Investment:  " + annualInvest.ToString("c") + "\t Annual Interest Rate: " + interestRate.ToString("#0.##%") + "\r\n\r\n");
-            
-            txtOutputArea.AppendText($"{"Year",10}" +
-                                     $"{"Age",10}" +
-                                     $"{"Beggining Balance",10}" +
-                                     $"{"Annual Investment",10}" +
-                                     $"{"Interest Earned",10}"+
-                                     $"{"Ending Balance",10}\r\n");
 
-            
-
-            for (int i = age; i <= retireAge; i++)
+            //Checks if current age is greater than retirement age
+            if (age > retireAge)
             {
-
-                txtOutputArea.AppendText($"{year,10}"+
-                    $"{age,10}"+
-                    $"{begginingBal.ToString("c"),14}"+
-                    $"{annualInvest.ToString("c"),14}"+
-                    $"{interestEarned.ToString("c"),14}"+
-                    $"{endingBal.ToString("c"),14}" + "\r\n");
-                Console.WriteLine("{0,10}{1,10}",
-  age,
-  begginingBal.ToString("c"));
-
-
-                interestEarned = begginingBal * interestRate;
-                endingBal = begginingBal + annualInvest + interestEarned;
-                begginingBal = endingBal;
-                age++;
-                year++;
-                
+                txtOutputArea.AppendText("Please check and make sure you set the age to the right number");
             }
+            else if (annualInvest == 0)
+            {
+                txtOutputArea.AppendText("You have to have an annual investment");
+            }
+            else
+            {
+                txtOutputArea.AppendText("IRA Summary Projection for " + clientName + "\r\n\r\n");
+                txtOutputArea.AppendText("Fixed Annual Investment:  " + annualInvest.ToString("c") + "\t Annual Interest Rate: " + interestRate.ToString("#0.####%") + "\r\n\r\n");
+
+                txtOutputArea.AppendText($"{"Year",10}" +
+                                         $"{"Age",10}" +
+                                         $"{"Beggining Balance",20}" +
+                                         $"{"Annual Investment",10}" +
+                                         $"{"Interest Earned",10}" +
+                                         $"{"Ending Balance",10}\r\n\r\n\r\n");
+
+
+                
+
+                for (int i = age; i <= retireAge; i++)
+                {
+
+                    txtOutputArea.AppendText($"{year,10}" +
+                                             $"{age,10}" +
+                                             $"{begginingBal,15:C}" +
+                                             $"{annualInvest,15:C}" +
+                                             $"{interestEarned,15:C}" +
+                                             $"{endingBal,15:C} \r\n");
+
+
+                    interestEarned = begginingBal * interestRate;
+                    endingBal = begginingBal + annualInvest + interestEarned;
+                    begginingBal = endingBal;
+                    age++;
+                    year++;
+
+                }
+
+            }
+            
+         
             
 
             
