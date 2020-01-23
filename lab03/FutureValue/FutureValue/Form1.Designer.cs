30fc0a2e4a670e490099e9b76d6c39b683119d42
harrisrl2023 <harrisrl2023@mountunion.edu>
2020-01-23 14:14:05 -0500

    Started lab03

---
+++ lab03/FutureValue/FutureValue/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace FutureValue
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtMonthlyInvest = new System.Windows.Forms.TextBox();
+            this.txtYears = new System.Windows.Forms.TextBox();
+            this.txtInterestRate = new System.Windows.Forms.TextBox();
+            this.txtFutureVal = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Monthly Investment: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Yearly Interest Rate:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Number of Years: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Future Value:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(40, 203);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 4;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(121, 203);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // txtMonthlyInvest
+            // 
+            this.txtMonthlyInvest.Location = new System.Drawing.Point(121, 17);
+            this.txtMonthlyInvest.Name = "txtMonthlyInvest";
+            this.txtMonthlyInvest.Size = new System.Drawing.Size(100, 20);
+            this.txtMonthlyInvest.TabIndex = 6;
+            // 
+            // txtYears
+            // 
+            this.txtYears.Location = new System.Drawing.Point(121, 101);
+            this.txtYears.Name = "txtYears";
+            this.txtYears.Size = new System.Drawing.Size(100, 20);
+            this.txtYears.TabIndex = 7;
+            // 
+            // txtInterestRate
+            // 
+            this.txtInterestRate.Location = new System.Drawing.Point(121, 64);
+            this.txtInterestRate.Name = "txtInterestRate";
+            this.txtInterestRate.Size = new System.Drawing.Size(100, 20);
+            this.txtInterestRate.TabIndex = 8;
+            // 
+            // txtFutureVal
+            // 
+            this.txtFutureVal.Location = new System.Drawing.Point(121, 143);
+            this.txtFutureVal.Name = "txtFutureVal";
+            this.txtFutureVal.ReadOnly = true;
+            this.txtFutureVal.Size = new System.Drawing.Size(100, 20);
+            this.txtFutureVal.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(271, 238);
+            this.Controls.Add(this.txtFutureVal);
+            this.Controls.Add(this.txtInterestRate);
+            this.Controls.Add(this.txtYears);
+            this.Controls.Add(this.txtMonthlyInvest);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtMonthlyInvest;
+        private System.Windows.Forms.TextBox txtYears;
+        private System.Windows.Forms.TextBox txtInterestRate;
+        private System.Windows.Forms.TextBox txtFutureVal;
+    }
+}
+
---
+++ lab03/FutureValue/FutureValue/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FutureValue
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+   
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            txtFutureVal.Text = String.Empty;
+            decimal monthlyInvest = Convert.ToDecimal(txtMonthlyInvest.Text);
+            int years = Convert.ToInt32(txtYears.Text);
+            decimal interestRate = Convert.ToDecimal(txtInterestRate.Text);
+            decimal futureValue = 0m;
+            decimal yearlyInvest = monthlyInvest * 12;
+            decimal begginingBalance = 0m;
+            decimal interestEarned = 0m;
+
+ 
+            for (int i = 0; i <= years; i++)
+            {
+                interestEarned = yearlyInvest * interestRate;
+                futureValue = begginingBalance + yearlyInvest + interestEarned;
+            }
+
+            txtFutureVal.Text = futureValue.ToString("c");
+
+
+        }
+    }
+}
