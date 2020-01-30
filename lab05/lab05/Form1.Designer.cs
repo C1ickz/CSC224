7c30eb71367d748622dc9148996b468d67ce8c35
harrisrl2023 <harrisrl2023@mountunion.edu>
2020-01-30 13:35:38 -0500

    finished lab05

--- lab05/lab05/Form1.Designer.cs
+++ lab05/lab05/Form1.Designer.cs
@@ -28,13 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnClearFileName = new System.Windows.Forms.Button();
+            this.btnReadInput = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File name (including path):";
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(148, 43);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(229, 20);
+            this.txtFileName.TabIndex = 1;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(15, 197);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(523, 218);
+            this.txtOutput.TabIndex = 2;
+            // 
+            // btnClearFileName
+            // 
+            this.btnClearFileName.Location = new System.Drawing.Point(383, 43);
+            this.btnClearFileName.Name = "btnClearFileName";
+            this.btnClearFileName.Size = new System.Drawing.Size(118, 23);
+            this.btnClearFileName.TabIndex = 3;
+            this.btnClearFileName.Text = "Clear FileName Field";
+            this.btnClearFileName.UseVisualStyleBackColor = true;
+            this.btnClearFileName.Click += new System.EventHandler(this.btnClearFileName_Click);
+            // 
+            // btnReadInput
+            // 
+            this.btnReadInput.Location = new System.Drawing.Point(194, 118);
+            this.btnReadInput.Name = "btnReadInput";
+            this.btnReadInput.Size = new System.Drawing.Size(137, 38);
+            this.btnReadInput.TabIndex = 4;
+            this.btnReadInput.Text = "Read Input File";
+            this.btnReadInput.UseVisualStyleBackColor = true;
+            this.btnReadInput.Click += new System.EventHandler(this.btnReadInput_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(550, 450);
+            this.Controls.Add(this.btnReadInput);
+            this.Controls.Add(this.btnClearFileName);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnClearFileName;
+        private System.Windows.Forms.Button btnReadInput;
     }
 }
 
--- lab05/lab05/Form1.cs
+++ lab05/lab05/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,54 @@ namespace lab05
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnClearFileName_Click(object sender, EventArgs e)
+        {
+            txtFileName.Text = String.Empty;
+        }
+
+        private void btnReadInput_Click(object sender, EventArgs e)
+        {
+            string filePath = txtFileName.Text;
+            FileStream fs = null;
+            try
+            {
+
+               fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+                StreamReader textIn = new StreamReader(fs);
+                txtOutput.Text = "Contents of file \"" + filePath + "\":\r\n==================\r\n";
+                string theLine;
+                int lines = 0;
+                int emptyLines = 0;
+                while(textIn.Peek() != -1)
+                {
+                    theLine = textIn.ReadLine();
+                    if(theLine == String.Empty)
+                    {
+                        emptyLines++;
+                    }
+                    else
+                    {
+                        txtOutput.AppendText(theLine + "\r\n");
+                        lines++;
+                    }
+                }
+                txtOutput.AppendText("================== end of file\r\n");
+                txtOutput.AppendText("There are " + lines + " lines with text and there are " + emptyLines + " empty lines");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\n File Reading error");
+            }
+            finally
+            {
+                if(fs != null)
+                {
+                    fs.Close();
+                }
+            }
         }
     }
 }
