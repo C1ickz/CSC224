c5f7d6915bc7b179562cb8f162c6d1c2acf4b0b1
harrisrl2023 <46679450+harrisrl2023@users.noreply.github.com>
2020-02-20 00:47:16 -0500

    Finished project04

---
+++ Project4/Project4/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Project4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInputFile = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input File:";
+            // 
+            // txtInputFile
+            // 
+            this.txtInputFile.Location = new System.Drawing.Point(97, 48);
+            this.txtInputFile.Name = "txtInputFile";
+            this.txtInputFile.Size = new System.Drawing.Size(323, 20);
+            this.txtInputFile.TabIndex = 2;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(443, 44);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(165, 27);
+            this.btnBrowse.TabIndex = 4;
+            this.btnBrowse.Text = "Browse for Input";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(64, 148);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(544, 258);
+            this.txtOutput.TabIndex = 5;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(97, 91);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(141, 23);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save to disk";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtInputFile);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Program 4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInputFile;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ Project4/Project4/Form1.cs
@@ -0,0 +1,125 @@
+﻿/******************************
+ * CSC 224
+ * Programming Assignment # 4
+ * Author:  Ryan Harris
+ * Date:  February 20, 2020
+ ******************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtInputFile.Text = fileDialog.FileName;
+    
+            }
+            string path = txtInputFile.Text;
+            string currentState = "";
+            string firstName = "";
+            string lastName = "";
+            string previousState = "";
+            decimal customerTotal = 0m;
+            decimal stateSubtotal = 0m;
+            int customersInState = 0;
+            int customersInAllStates = 0;
+            decimal total = 0m;
+
+            txtOutput.AppendText("CSC 224 - Program # 4\r\n");
+            txtOutput.AppendText("Written by:  Ryan\r\n\r\n");
+            try
+            {
+    
+                using (StreamReader sr = new StreamReader(path))
+                {
+              
+                    string line;
+
+                    while ((line = sr.ReadLine()) != null)
+                    {
+               
+                        currentState = line.Split(';')[0];
+                        firstName = (line.Split(';')[1]).Split(',')[1];
+                        lastName = (line.Split(';')[1]).Split(',')[0];
+
+                        customerTotal = Convert.ToDecimal(line.Split(';')[2]);
+
+                        if(customersInAllStates == 0)
+                        {
+                            txtOutput.AppendText("Customers from " + currentState + ":\r\n");
+                            previousState = currentState;
+
+                        }
+
+
+                        if (!currentState.Equals(previousState))
+                        {
+                            txtOutput.AppendText("\r\n");
+                            txtOutput.AppendText("\t Number of customers from " + previousState + ": " + customersInState + 
+                                "    (total due from " + previousState + " customers: " + stateSubtotal.ToString("c") +")\r\n");
+                            txtOutput.AppendText("\r\nCustomers from " + currentState + ":\r\n");
+                            stateSubtotal = customersInState= 0;
+                            stateSubtotal = 0m;
+                        }
+                        txtOutput.AppendText("\t" + firstName + " " + lastName + " (balance due = " + customerTotal.ToString("c") + ")\r\n");
+
+                        customersInState++;
+                        customersInAllStates++;
+                        stateSubtotal += customerTotal;
+                        previousState = currentState;
+                        total += customerTotal;
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("File formatted incorrectly", "File format error");
+            }
+            finally
+            {
+                txtOutput.AppendText("\r\n");
+                txtOutput.AppendText("\t Number of customers from " + previousState + ": " + customersInState +
+                    "    (total due from " + previousState + " customers: " + stateSubtotal.ToString("c") + ")\r\n");
+                txtOutput.AppendText("\r\nTotal Customers from all states: " + customersInAllStates + "\r\n\r\n");
+                txtOutput.AppendText("\t Total Due from ALL customers: " + total.ToString("c"));
+
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string outputFile = txtInputFile.Text.Substring(0, txtInputFile.Text.LastIndexOf(".")) + ".out";
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(outputFile))
+                {
+                    sw.WriteLine(txtOutput.Text);
+                    MessageBox.Show("File succesfully saved", "File save confirmation");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error when attempting to save the file", "File save error")
+            }
+        }
+    }
+}
